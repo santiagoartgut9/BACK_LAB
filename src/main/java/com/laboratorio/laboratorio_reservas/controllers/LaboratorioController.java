6ccd8fe99b7a0abad6e169b392de085c1c8bc756
santiagoartgut9 <69946591+santiagoartgut9@users.noreply.github.com>
2025-04-03 11:52:15 -0500

    Update LaboratorioController.java

--- src/main/java/com/laboratorio/laboratorio_reservas/controllers/LaboratorioController.java
+++ src/main/java/com/laboratorio/laboratorio_reservas/controllers/LaboratorioController.java
@@ -7,14 +7,28 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = {"http://localhost:3001", "https://ambitious-river-02ff82c00.6.azurestaticapps.net"}) 
+/**
+ * Controlador REST para gestionar los laboratorios.
+ * 
+ * Permite obtener la lista de laboratorios disponibles en el sistema.
+ * Habilita el acceso desde dominios externos mediante CORS.
+ */
+@CrossOrigin(origins = {"http://localhost:3001", "https://ambitious-river-02ff82c00.6.azurestaticapps.net"})
 @RestController
 @RequestMapping("/laboratorios")
 public class LaboratorioController {
 
+    /**
+     * Servicio que maneja la lógica de negocio relacionada con los laboratorios.
+     */
     @Autowired
     private LaboratorioService laboratorioService;
 
+    /**
+     * Obtiene la lista de todos los laboratorios disponibles.
+     * 
+     * @return Lista de objetos {@link Laboratorio} que representan los laboratorios en la base de datos.
+     */
     @GetMapping
     public List<Laboratorio> obtenerLaboratorios() {
         return laboratorioService.listarLaboratorios();
